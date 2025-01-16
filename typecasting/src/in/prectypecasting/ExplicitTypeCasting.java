df3e331e188bb2cd14d3f17cabee65b5eda729b0
DELL <DELL@DESKTOP-VMTGHJK>
2025-01-16 07:42:08 -0800

    Explicit Type casting, Implicit Type casting

---
+++ typecasting/src/in/prectypecasting/ExplicitTypeCasting.java
@@ -0,0 +1,12 @@
+package in.prectypecasting;
+
+public class ExplicitTypeCasting {
+
+	public static void main (String[] args) {
+		//explicit type casting /manually done by developer/narrowing
+		double number1=14.5;
+		int number2=(int)number1;
+		System.out.println(number2);
+	}
+
+}
---
+++ typecasting/src/in/prectypecasting/ImplicitTypeCasting.java
@@ -0,0 +1,14 @@
+package in.prectypecasting;
+
+public class ImplicitTypeCasting {
+
+	
+	public static void main(String[] args) {
+		// implicit type casting/widening/down casting
+	    int variable=40;
+    	float variable1=variable;
+		
+	System.out.println(variable1);
+		}
+
+}
